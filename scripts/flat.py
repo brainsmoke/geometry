67f7e079f1586e9a8e4da2646dd51db961ee7af4
Erik Bosman <erik@minemu.org>
2023-07-30 20:46:11 +0200

    add argparse to flat.py

---
+++ scripts/flat.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys, cmath, argparse
+
+import kit, scad, plot
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+#group = parser.add_mutually_exclusive_group(required=True)
+#group.add_argument('-scad', action='store_true')
+#group.add_argument('-svg', action='store_true')
+
+parser.add_argument('-tiling', type=str, default='penta', choices=['tri', 'penta'], help="kind of tiling")
+parser.add_argument('-r', type=float, default=250., help="outer radius, measured at the midpoint of the facet")
+parser.add_argument('-width', type=float, default=22., help="tube width")
+parser.add_argument('-subdivisions', type=int, default=1, help="number of subdivisions")
+parser.add_argument('-thickness', type=float, default=3., help="material thickness")
+parser.add_argument('-notch-depth', type=float, default=10, help="notch depth")
+parser.add_argument('-cut-width', type=float, default=.3, help="laser cutter's cutting width")
+
+args = parser.parse_args()
+
+kind = args.tiling
+r = args.r
+width = args.width
+subdivisions= args.subdivisions
+notch_depth = args.notch_depth
+thickness=args.thickness
+cutwidth=args.cut_width
+
+shapes, shape_desc = kit.flat(kind, r, width, subdivisions, thickness, notch_depth)
+
+plot.start(cutwidth=cutwidth)
+
+for points, desc in zip(shapes, shape_desc):
+    plot.plot([points], desc)
+ 
+plot.end()
+
