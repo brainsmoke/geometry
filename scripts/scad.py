a2097163b0ca27dc6109dd06a62be48aa03fb74b
Erik Bosman <erik@minemu.org>
2023-07-30 16:32:00 +0200

    delete leftover code

--- scripts/scad.py
+++ scripts/scad.py
@@ -2,20 +2,6 @@
 
 import sys, cmath
 
-import kit
-
-kind = (sys.argv[1:] + ['penta'])[0]
-assert kind in ('penta', 'tri')
-r = 250 # outer radius, measured at the midpoint of the facet
-width = 18
-height = 18
-subdivisions=2
-notch_width = 3
-
-thickness=3
-
-shapes, shape_desc = kit.tube(kind, r, width, height, subdivisions, thickness, notch_width)
-
 tubearc = """
 
 module subdiv_arc(a, n)
